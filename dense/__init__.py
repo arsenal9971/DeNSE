74a95d8efe57415a7ae0dcf03e94c97fc1d5ccb6
Hector Andrade Loarca <andrade@kepler.math.tu-berlin.de>
2019-01-28 18:36:09 +0100

    Adding the results and code

---
+++ dense/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import
+
+__all__ = ()
+
+from .batchgen import *
+__all__ += batchgen.__all__
+
+from .shearlab import *
+__all__ += shearlab.__all__
+
+from .datagen import *
+__all__ += datagen.__all__
+
